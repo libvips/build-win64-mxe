7753dd20e417a5b881a0df781a3f606370672515
Kleis Auke Wolthuizen <github@kleisauke.nl>
2019-04-11 18:25:31 +0200

    Cherry-pick merge b7878d5 into 8.8

--- 8.8/vips-all.mk
+++ 8.8/vips-all.mk
@@ -14,6 +14,51 @@ define $(PKG)_PRE_CONFIGURE
     # Copy some files to the packaging directory
     mkdir -p $(TOP_DIR)/vips-packaging
     $(foreach f,COPYING ChangeLog README.md AUTHORS, mv '$(SOURCE_DIR)/$f' '$(TOP_DIR)/vips-packaging';)
+
+    (echo '{'; \
+     echo '  "cairo": "$(cairo_VERSION)",'; \
+     echo '  "cfitsio": "$(cfitsio_VERSION)",'; \
+     echo '  "croco": "$(libcroco_VERSION)",'; \
+     echo '  "de265": "$(libde265_VERSION)",'; \
+     echo '  "exif": "$(libexif_VERSION)",'; \
+     echo '  "expat": "$(expat_VERSION)",'; \
+     echo '  "ffi": "$(libffi_VERSION)",'; \
+     echo '  "fftw": "$(fftw_VERSION)",'; \
+     echo '  "fontconfig": "$(fontconfig_VERSION)",'; \
+     echo '  "freetype": "$(freetype_VERSION)",'; \
+     echo '  "fribidi": "$(fribidi_VERSION)",'; \
+     echo '  "gdkpixbuf": "$(gdk-pixbuf_VERSION)",'; \
+     echo '  "gettext": "$(gettext_VERSION)",'; \
+     echo '  "gif": "$(giflib_VERSION)",'; \
+     echo '  "glib": "$(glib_VERSION)",'; \
+     echo '  "gsf": "$(libgsf_VERSION)",'; \
+     echo '  "harfbuzz": "$(harfbuzz_VERSION)",'; \
+     echo '  "hdf5": "$(hdf5_VERSION)",'; \
+     echo '  "heif": "$(libheif_VERSION)",'; \
+     echo '  "iconv": "$(libiconv_VERSION)",'; \
+     echo '  "imagemagick": "$(imagemagick_VERSION)",'; \
+     echo '  "jpeg": "$(libjpeg-turbo_VERSION)",'; \
+     echo '  "lcms": "$(lcms_VERSION)",'; \
+     echo '  "matio": "$(matio_VERSION)",'; \
+     echo '  "nifti": "$(nifticlib_VERSION)",'; \
+     echo '  "openexr": "$(openexr_VERSION)",'; \
+     echo '  "openjpeg": "$(openjpeg_VERSION)",'; \
+     echo '  "openslide": "$(openslide_VERSION)",'; \
+     echo '  "orc": "$(orc_VERSION)",'; \
+     echo '  "pango": "$(pango_VERSION)",'; \
+     echo '  "pixman": "$(pixman_VERSION)",'; \
+     echo '  "png": "$(libpng_VERSION)",'; \
+     echo '  "poppler": "$(poppler_VERSION)",'; \
+     echo '  "sqlite": "$(sqlite_VERSION)",'; \
+     echo '  "svg": "$(librsvg_VERSION)",'; \
+     echo '  "tiff": "$(tiff_VERSION)",'; \
+     echo '  "vips": "$(vips-web_VERSION)",'; \
+     echo '  "webp": "$(libwebp_VERSION)",'; \
+     echo '  "x265": "$(x265_VERSION)",'; \
+     echo '  "xml": "$(libxml2_VERSION)",'; \
+     echo '  "zlib": "$(zlib_VERSION)"'; \
+     echo '}';) \
+     > '$(TOP_DIR)/vips-packaging/versions.json'
 endef
 
 define $(PKG)_BUILD
