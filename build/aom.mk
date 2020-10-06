219b2f3710a0a7245032e1a6b6143744c8ba257d
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-10-06 14:48:49 +0200

    aom: switch to new release tarball
    See https://bugs.chromium.org/p/aomedia/issues/detail?id=2743#c5

--- build/aom.mk
+++ build/aom.mk
@@ -3,13 +3,10 @@ $(PKG)_WEBSITE  := https://aomedia.googlesource.com/aom/
 $(PKG)_DESCR    := AV1 Codec Library
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.0.0
-$(PKG)_CHECKSUM := 3567ebe767e469f13bb25a334b964acb20ff79dd26510a4e7367e0ab0c289898
+$(PKG)_CHECKSUM := 7be566ad19209f076a4b22b54f6638651ea7131c06c33eda1152552da23f4597
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-# We use a local mirror from FreeBSD because
-# downloading from googlesource is not reliable.
-$(PKG)_GH_CONF  := jbeich/aom/tags,v
-# $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-# $(PKG)_URL      := https://aomedia.googlesource.com/aom/+archive/v$($(PKG)_VERSION).tar.gz
+$(PKG)_FILE     := libaom-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://storage.googleapis.com/aom-releases/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc $(BUILD)~nasm
 
 define $(PKG)_BUILD
