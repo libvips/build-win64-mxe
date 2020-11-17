2c4c6ed6833515df56ff826d87a91b3fe5a4e444
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-11-17 16:05:54 +0100

    [WiP] Build orc from the master branch
    Since AArch64 support recently landed on the master branch.
    
    See https://github.com/bleroy/core-imaging-playground/pull/21

--- build/orc.mk
+++ build/orc.mk
@@ -2,12 +2,13 @@ PKG             := orc
 $(PKG)_WEBSITE  := https://gstreamer.freedesktop.org/modules/orc.html
 $(PKG)_DESCR    := Library and set of tools for compiling and executing SIMD assembly language-like programs that operate on arrays of data.
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.4.32
-$(PKG)_CHECKSUM := a66e3d8f2b7e65178d786a01ef61f2a0a0b4d0b8370de7ce134ba73da4af18f0
+$(PKG)_FULL_SHA := 8d32f2d9f4a803e8e27c2f26ab54233d03efd023
+$(PKG)_VERSION  := $(call substr,$($(PKG)_FULL_SHA),1,8)
+$(PKG)_CHECKSUM := 25e338290db9e1b7ae69a3d8e999d47f315117194ef3cca87c5a34b1a1ad220f
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://gstreamer.freedesktop.org/src/$(PKG)/$($(PKG)_FILE)
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)-$($(PKG)_FULL_SHA)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://gitlab.freedesktop.org/api/v4/projects/1360/repository/archive?sha=$($(PKG)_VERSION)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
