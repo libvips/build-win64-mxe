f44a178c2c6720ab08ea9a75a994491de85d8284
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-06-09 20:29:43 +0200

    Update highway to google/highway@e8ab731

--- build/highway.mk
+++ build/highway.mk
@@ -2,9 +2,9 @@ PKG             := highway
 $(PKG)_WEBSITE  := https://github.com/google/highway
 $(PKG)_DESCR    := Performance-portable, length-agnostic SIMD with runtime dispatch
 $(PKG)_IGNORE   :=
-# https://github.com/google/highway/tarball/96ece0b038318964be63685b6f2100da716dd60d
-$(PKG)_VERSION  := 96ece0b
-$(PKG)_CHECKSUM := 36c7dd6ec3a5f9aadfb25918b8b7ffb475b720d7eec849147ab002d7a4fab3ce
+# https://github.com/google/highway/tarball/e8ab731e4eff6df5415771518345af85f9bffa0f
+$(PKG)_VERSION  := e8ab731
+$(PKG)_CHECKSUM := f4ff871a60fa7f102e947d0d563f550eff1cc22afc6a6343d2223de85563b31b
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/highway-[0-9]*.patch)))
 $(PKG)_GH_CONF  := google/highway/branches/master
 $(PKG)_DEPS     := cc
