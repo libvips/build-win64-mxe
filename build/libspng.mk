fdbf3cab518868b865544bec1ffabf2907ebecf0
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-02-10 19:33:29 +0100

    Test build for randy408/libspng@4baa1b1

--- build/libspng.mk
+++ build/libspng.mk
@@ -2,10 +2,11 @@ PKG             := libspng
 $(PKG)_WEBSITE  := https://libspng.org/
 $(PKG)_DESCR    := Simple, modern libpng alternative.
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.7.1
-$(PKG)_CHECKSUM := 0726a4914ad7155028f3baa94027244d439cd2a2fbe8daf780c2150c4c951d8e
+# https://github.com/randy408/libspng/tarball/4baa1b1384da4ae9ddbe42a90e5f6300afb44fe7
+$(PKG)_VERSION  := 4baa1b1
+$(PKG)_CHECKSUM := e103bafb4e40e93fe2bed3ed83aa471c9a11429737b067e2b7210b8ed3601577
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := randy408/libspng/tags,v
+$(PKG)_GH_CONF  := randy408/libspng/branches/master
 $(PKG)_DEPS     := cc zlib
 
 define $(PKG)_BUILD
