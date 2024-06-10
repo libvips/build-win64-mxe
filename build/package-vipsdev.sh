24c7d64041cf544b7b984157b62e97aaa19419d2
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-06-10 21:16:11 +0200

    Link against debug version of the UCRT
    i.e. when building with `--with-debug`.

--- build/package-vipsdev.sh
+++ build/package-vipsdev.sh
@@ -119,9 +119,14 @@ fi
 
 echo "Copying libvips and dependencies"
 
-# Whitelist the API set DLLs
-# Can't do api-ms-win-crt-*-l1-1-0.dll, unfortunately
-whitelist=(api-ms-win-crt-{conio,convert,environment,filesystem,heap,locale,math,multibyte,private,process,runtime,stdio,string,time,utility}-l1-1-0.dll)
+if [ "$DEBUG" = "true" ]; then
+  # Whitelist ucrtbased.dll for debug builds
+  whitelist=(ucrtbased.dll)
+else
+  # Whitelist the API set DLLs
+  # Can't do api-ms-win-crt-*-l1-1-0.dll, unfortunately
+  whitelist=(api-ms-win-crt-{conio,convert,environment,filesystem,heap,locale,math,multibyte,private,process,runtime,stdio,string,time,utility}-l1-1-0.dll)
+fi
 
 # Whitelist bcryptprimitives.dll for Rust
 # See: https://github.com/rust-lang/rust/pull/84096
