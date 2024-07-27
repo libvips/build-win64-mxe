f5b21ad698b0a10bba8d68c9ff29f8fdbb102987
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-07-27 10:39:21 +0200

    Test build for LLVM 19.1.0-rc1
    - Update MinGW-w64 to the latest master version.

--- build/plugins/llvm-mingw/compiler-rt-sanitizers.mk
+++ build/plugins/llvm-mingw/compiler-rt-sanitizers.mk
@@ -2,7 +2,7 @@
 
 PKG             := compiler-rt-sanitizers
 $(PKG)_WEBSITE  := https://compiler-rt.llvm.org/
-$(PKG)_VERSION  := 18.1.8
+$(PKG)_VERSION  := 19.1.0-rc1
 $(PKG)_DEPS     := cc
 $(PKG)_TYPE     := meta
 
