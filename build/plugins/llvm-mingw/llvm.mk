ab25c60ca5da45e83769a26b5da527e2dee93161
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-08-06 09:43:06 +0200

    Test build for LLVM 19.1.0-rc2
    - Update MinGW-w64 to the latest master version.

--- build/plugins/llvm-mingw/llvm.mk
+++ build/plugins/llvm-mingw/llvm.mk
@@ -5,8 +5,8 @@ $(PKG)_WEBSITE  := https://llvm.org/
 $(PKG)_DESCR    := A collection of modular and reusable compiler and toolchain technologies
 $(PKG)_IGNORE   :=
 # This version needs to be in-sync with the compiler-rt-sanitizers package
-$(PKG)_VERSION  := 19.1.0-rc1
-$(PKG)_CHECKSUM := 870175897330e9f1f17c819f5999f36538e20955e25b14252bf932bc8f9ec776
+$(PKG)_VERSION  := 19.1.0-rc2
+$(PKG)_CHECKSUM := e6a72922f6999ce02a1da46059b479fc15fbd474eef0f3689a28cede0e8f1d86
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/llvm-[0-9]*.patch)))
 $(PKG)_GH_CONF  := llvm/llvm-project/releases/latest,llvmorg-,,,,.tar.xz
 $(PKG)_SUBDIR   := $(PKG)-project-$($(PKG)_VERSION).src
