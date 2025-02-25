6fd70b89b9e558eea19834f19cf6f8d02599039d
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-02-25 19:44:29 +0100

    Update for 9.0.1-4

--- build/nip4.mk
+++ build/nip4.mk
@@ -2,10 +2,10 @@ PKG             := nip4
 $(PKG)_WEBSITE  := https://github.com/jcupitt/nip4
 $(PKG)_DESCR    := Image processing spreadsheet
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 9.0.1-3
-$(PKG)_CHECKSUM := bdb2e16e123d378e4de631becbafae60e8f4196ca5c9769c92cc10c04dbf39ca
+$(PKG)_VERSION  := 9.0.1-4
+$(PKG)_CHECKSUM := 54e3309f5109668321a874c6d798112599495e9ce2a2092d259b41c57185b0a2
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := jcupitt/nip4/releases,v,,,,.tar.xz
+$(PKG)_GH_CONF  := jcupitt/nip4/tags,v
 $(PKG)_DEPS     := cc meson-wrapper gtk4 gsl $(foreach TARGET,$(MXE_TARGETS),vips-$(lastword $(call split,.,$(TARGET))))
 
 define $(PKG)_PRE_CONFIGURE
