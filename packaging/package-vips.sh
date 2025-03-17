libvips/build-win64-mxe

commit 6a47c506c2fe3e43c8bf356772ebe6c5b2790729

Minor packaging improvements

packaging/package-vips.sh
@@ -5,7 +5,7 @@ set -e
 
 if [[ "$*" == *--help* ]]; then
   cat <<EOF
-Usage: $(basename "$0") [OPTIONS] [DEPS] [MXE_TARGET]
+Usage: $(basename "$0") [OPTIONS] [DEPS] [TARGET]
 Package libvips in /usr/local/mxe/usr/TARGET/
 
 OPTIONS:
@@ -18,7 +18,7 @@ DEPS:
 	    - web
 	    - all
 
-MXE_TARGET:
+TARGET:
 	The binary target,
 	    defaults to 'x86_64-w64-mingw32.shared'
 	Possible values are:
@@ -56,8 +56,8 @@ strip=$target-strip
 vips_version=$(jq -r ".vips" $mxe_prefix/$target/vips-packaging/versions.json)
 
 # Directories
-repackage_dir=vips-dev-$(without_patch $vips_version)
-pdb_dir=vips-pdb-$(without_patch $vips_version)
+repackage_dir=/var/tmp/vips-dev-$(without_patch $vips_version)
+pdb_dir=/var/tmp/vips-pdb-$(without_patch $vips_version)
 install_dir=$mxe_prefix/$target
 bin_dir=$install_dir/bin
 lib_dir=$install_dir/lib

packaging/package-vipsdisp.sh
@@ -5,13 +5,13 @@ set -e
 
 if [[ "$*" == *--help* ]]; then
   cat <<EOF
-Usage: $(basename "$0") [OPTIONS] [MXE_TARGET]
+Usage: $(basename "$0") [OPTIONS] [TARGET]
 Package vipsdisp in /usr/local/mxe/usr/TARGET/
 
 OPTIONS:
 	--help	Show the help and exit
 
-MXE_TARGET:
+TARGET:
 	The binary target,
 	    defaults to 'x86_64-w64-mingw32.shared'
 	Possible values are:
@@ -40,8 +40,8 @@ strip=$target-strip
 package_version=$(jq -r ".vipsdisp" $mxe_prefix/$target/vips-packaging/versions-vipsdisp.json)
 
 # Directories
-repackage_dir=vipsdisp
-pdb_dir=vipsdisp-pdb
+repackage_dir=/var/tmp/vipsdisp
+pdb_dir=/var/tmp/vipsdisp-pdb
 install_dir=$mxe_prefix/$target
 bin_dir=$install_dir/bin
 lib_dir=$install_dir/lib

.gitignore
@@ -1,6 +1,5 @@
 # Build output
-packaging/vips*
-packaging/vipsdisp*
+*.zip
 
 # VIM swap files
 .*.swp

build.sh
@@ -11,7 +11,7 @@ Build Windows binaries for libvips in a container
 
 OPTIONS:
 	--help			Show the help and exit
-	-t, --target <MXE_TARGET>	The binary target (this can be specified multiple times)
+	-t, --target <TARGET>	The binary target (this can be specified multiple times)
 	-c, --commit <COMMIT>	The commit to build libvips from
 	-r, --ref <REF>		The branch or tag to build libvips from
 	--nightly		Build libvips from tip-of-tree (alias of -r master)
@@ -27,7 +27,7 @@ PKGS:
 	The packages and their dependencies to build,
 	    defaults to 'vips-web'
 
-MXE_TARGET:
+TARGET:
 	The binary target,
 	    defaults to building for all possible targets
 	Possible values are:

container/Dockerfile
@@ -25,14 +25,6 @@ RUN --mount=type=cache,id=mxe-download,target=/usr/local/mxe/pkg \
     MXE_TARGETS="$MXE_TARGETS" \
     GIT_COMMIT=$GIT_COMMIT
 
-# Build and bundle llvm-mingw tests when debugging
-RUN --mount=type=cache,id=mxe-download,target=/usr/local/mxe/pkg \
-  if [ "$DEBUG" = true ]; then \
-    make test-llvm-mingw \
-      MXE_PLUGIN_DIRS="$PLUGIN_DIRS" \
-      MXE_TARGETS="$MXE_TARGETS"; \
-  fi
-
 # The packaging dir is mounted here
 WORKDIR /data/packaging
 

packaging/gendeflibs.sh
@@ -5,13 +5,13 @@ set -e
 
 if [[ "$*" == *--help* ]]; then
   cat <<EOF
-Usage: $(basename "$0") [OPTIONS] [MXE_TARGET]
+Usage: $(basename "$0") [OPTIONS] [TARGET]
 Make .def and .lib files for all the DLLs we've built
 
 OPTIONS:
 	--help	Show the help and exit
 
-MXE_TARGET:
+TARGET:
 	The binary target,
 	    defaults to 'x86_64-w64-mingw32.shared'
 	Possible values are:

packaging/package.sh
@@ -12,6 +12,10 @@ for pkg in $PKGS; do
     for target in $MXE_TARGETS; do
       ./package-vispdisp.sh $target
     done
+  elif [ "$pkg" = "test-llvm-mingw" ]; then
+    for target in $MXE_TARGETS; do
+      cp /usr/local/mxe/usr/$target/bin/test-llvm-mingw.zip test-llvm-mingw-$target.zip
+    done
   else
     echo "WARNING: Skipping packaging script for '$pkg'." >&2
   fi

commit 4df42534661d19b800589043dbf55bc2eab6dee8

Exclude parent directory structure from archive
Regressed since: 6a47c506c2fe3e43c8bf356772ebe6c5b2790729.

packaging/package-vips.sh
@@ -183,11 +183,11 @@ zipfile=vips-dev-$arch-$deps-$zip_suffix.zip
 echo "Creating $zipfile"
 
 rm -f $zipfile
-zip -r -qq $zipfile $repackage_dir
+(cd /var/tmp && zip -r -qq $work_dir/$zipfile $(basename $repackage_dir))
 
 zipfile=vips-pdb-$arch-$deps-$zip_suffix.zip
 
 echo "Creating $zipfile"
 
 rm -f $zipfile
-zip -r -qq $zipfile $pdb_dir
+(cd /var/tmp && zip -r -qq $work_dir/$zipfile $(basename $pdb_dir))

packaging/package-vipsdisp.sh
@@ -149,11 +149,11 @@ zipfile=vipsdisp-$arch-$zip_suffix.zip
 echo "Creating $zipfile"
 
 rm -f $zipfile
-zip -r -qq $zipfile $repackage_dir
+(cd $repackage_dir && zip -r -qq $work_dir/$zipfile .)
 
 zipfile=vipsdisp-pdb-$arch-$zip_suffix.zip
 
 echo "Creating $zipfile"
 
 rm -f $zipfile
-zip -r -qq $zipfile $pdb_dir
+(cd $pdb_dir && zip -r -qq $work_dir/$zipfile .)