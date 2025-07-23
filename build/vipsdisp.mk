937300faaeb5773d52914c44f1cc9d06aec8a0c7
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-07-23 19:41:20 +0200

    [Temp] Update vipsdisp to jcupitt/vipsdisp@2e0d67b

--- build/vipsdisp.mk
+++ build/vipsdisp.mk
@@ -2,10 +2,11 @@ PKG             := vipsdisp
 $(PKG)_WEBSITE  := https://github.com/jcupitt/vipsdisp
 $(PKG)_DESCR    := Tiny libvips / gtk+4 image viewer
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.0.0
-$(PKG)_CHECKSUM := 7bbb6740b13d0b211af2efab83d3a0d6e4646b15f57a038ac44ad67f446c5b64
+# https://github.com/jcupitt/vipsdisp/tarball/2e0d67b2a505765c08358f7b1954be2ead1e44ec
+$(PKG)_VERSION  := 2e0d67b
+$(PKG)_CHECKSUM := 1d43d0c53e60afb2e4f71d3aa53765185521f92f396121216fe7d7143c886429
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,.tar.xz
+$(PKG)_GH_CONF  := jcupitt/vipsdisp/branches/master
 $(PKG)_DEPS     := cc meson-wrapper gtk4 vips-all
 
 define $(PKG)_PRE_CONFIGURE
