0e2feb2a31dea4d77c6f8e78caaffe0247253e84
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-07-27 16:55:17 +0200

    fixup! update to 4.1.0 final

--- build/vipsdisp.mk
+++ build/vipsdisp.mk
@@ -5,9 +5,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.1.0
 $(PKG)_CHECKSUM := 51a1105f27e495fdd8e55a8628c4f688f28f900475efc479c9a7202ba59f09c2
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,-rc1.tar.xz
-$(PKG)_SUBDIR   := $(PKG)-$(firstword $(subst -, ,$($(PKG)_VERSION)))
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
+$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper gtk4 vips-all
 
 define $(PKG)_PRE_CONFIGURE
