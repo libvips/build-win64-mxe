16a658063924daa491dddbcfabb512629f22e68e
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-07-31 15:51:42 +0200

    Update vipsdisp to 4.1.0-high-dpi2

--- build/vipsdisp.mk
+++ build/vipsdisp.mk
@@ -2,10 +2,12 @@ PKG             := vipsdisp
 $(PKG)_WEBSITE  := https://github.com/jcupitt/vipsdisp
 $(PKG)_DESCR    := Tiny libvips / gtk+4 image viewer
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.1.0
-$(PKG)_CHECKSUM := 51a1105f27e495fdd8e55a8628c4f688f28f900475efc479c9a7202ba59f09c2
+$(PKG)_VERSION  := 4.1.0-high-dpi2
+$(PKG)_CHECKSUM := 4747b4bafacb7cd8ef8ca0cddeaa1ceb224578d5ab2e2f90c0ffc3b5fc335808
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,.tar.xz
+$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,-high-dpi2.tar.xz
+$(PKG)_SUBDIR   := $(PKG)-$(firstword $(subst -, ,$($(PKG)_VERSION)))
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_DEPS     := cc meson-wrapper gtk4 vips-all
 
 define $(PKG)_PRE_CONFIGURE
