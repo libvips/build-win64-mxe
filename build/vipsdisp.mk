829d13d1f247bc42371720189d550d1055836f72
John Cupitt <jcupitt@gmail.com>
2025-08-02 05:08:15 +0100

    vipsdisp has an icon

--- build/vipsdisp.mk
+++ build/vipsdisp.mk
@@ -2,10 +2,12 @@ PKG             := vipsdisp
 $(PKG)_WEBSITE  := https://github.com/jcupitt/vipsdisp
 $(PKG)_DESCR    := Tiny libvips / gtk+4 image viewer
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.1.1
-$(PKG)_CHECKSUM := 2a3378ab2f0e427effdcaab5025580e60c2937b04bbecb2a1a9346adc48dbe10
+$(PKG)_VERSION  := 4.1.1-windows-icon
+$(PKG)_CHECKSUM := 53db607dfb9d61c904514c4f64a7d5a5d856a451ba603b584868aea03c8a31f0
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,.tar.xz
+$(PKG)_GH_CONF  := jcupitt/vipsdisp/releases,v,,,,-windows-icon4.tar.xz
+$(PKG)_SUBDIR   := $(PKG)-$(firstword $(subst -, ,$($(PKG)_VERSION)))
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)-windows-icon4.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper gtk4 adwaita-icon-theme vips-all 
 
 define $(PKG)_PRE_CONFIGURE
