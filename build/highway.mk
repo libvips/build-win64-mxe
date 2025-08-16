251d67ed0eeeac8fa12bfe9c84c669ef39f6ae1f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-08-16 10:55:23 +0200

    Dependency version bumps
    - Update GLib to 2.85.3.
    - Update HarfBuzz to 11.4.1.
    - Update highway to 1.3.0.
    - Update libffi to 3.5.2.
    - Update libheif to 1.20.2.
    - Update librsvg to 2.61.0.
    - Update zlib-ng 2.2.5.
    - Update ImageMagick to 6.9.13-27.
    - Update Poppler to 25.08.0.
    
    (cherry picked from commit a19836179031b44bc3a1c85773e38edf1b8fe8d4)

--- build/highway.mk
+++ build/highway.mk
@@ -2,13 +2,12 @@ PKG             := highway
 $(PKG)_WEBSITE  := https://github.com/google/highway
 $(PKG)_DESCR    := Performance-portable, length-agnostic SIMD with runtime dispatch
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.0
-$(PKG)_CHECKSUM := 58e9d5d41d6573ad15245ad76aec53a69499ca7480c092d899c4424812ed906f
+$(PKG)_VERSION  := 1.3.0
+$(PKG)_CHECKSUM := e8d696900b45f4123be8a9d6866f4e7b6831bf599f4b9c178964d968e6a58a69
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
 $(PKG)_GH_CONF  := google/highway/releases
 $(PKG)_DEPS     := cc
 
-# Dynamic dispatch requires Linux to detect CPU capabilities on AArch64.
 define $(PKG)_BUILD
     $(eval export CFLAGS += -O3)
     $(eval export CXXFLAGS += -O3)
