93add934d2c745c1bab5258692078f5d22e88f1e
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-10-02 11:39:56 +0200

    Dependency version bumps
    - Update HarfBuzz to 12.1.0.
    - Update MozJPEG to mozilla/mozjpeg@0826579.
    - Update GDK-PixBuf to 2.44.3.
    - Update libjpeg-turbo to 3.1.2.

--- build/plugins/mozjpeg/mozjpeg.mk
+++ build/plugins/mozjpeg/mozjpeg.mk
@@ -2,10 +2,11 @@ PKG             := mozjpeg
 $(PKG)_WEBSITE  := https://github.com/mozilla/mozjpeg
 $(PKG)_DESCR    := A JPEG codec that provides increased compression for JPEG images (libjpeg-turbo fork).
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.1.5
-$(PKG)_CHECKSUM := 9fcbb7171f6ac383f5b391175d6fb3acde5e64c4c4727274eade84ed0998fcc1
+# https://github.com/mozilla/mozjpeg/tarball/08265790774cd0714832c9e675522acbe5581437
+$(PKG)_VERSION  := 0826579
+$(PKG)_CHECKSUM := 375d19302f09dfa267651dcfd3ddb62862ab3a488862b6168ce755bb1a925860
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/patches/$(PKG)-[0-9]*.patch)))
-$(PKG)_GH_CONF  := mozilla/mozjpeg/tags,v
+$(PKG)_GH_CONF  := mozilla/mozjpeg/branches/master
 $(PKG)_DEPS     := cc $(BUILD)~nasm
 
 # WITH_TURBOJPEG=OFF turns off a library we don't use (we just use the
@@ -14,8 +15,6 @@ define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
         -DWITH_TURBOJPEG=OFF \
         -DPNG_SUPPORTED=OFF \
-        -DENABLE_SHARED=$(CMAKE_SHARED_BOOL) \
-        -DENABLE_STATIC=$(CMAKE_STATIC_BOOL) \
         -DCMAKE_ASM_NASM_COMPILER='$(PREFIX)/$(BUILD)/bin/nasm' \
         '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
